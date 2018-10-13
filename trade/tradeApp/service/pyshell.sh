7c476da2de747cd3efce14d959eb5da6f8035701
shuhei.kenzaki <toto.at.home5@gmail.com>
2018-10-13 13:06:39 +0900

    add

--- trade/tradeApp/service/pyshell.sh
+++ trade/tradeApp/service/pyshell.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 count=`ps aux | grep -v grep | grep controller.py | wc -l`
-count_xrp=`ps aux | grep -v grep | grep controller_xrp.py | wc -l`
 
 if [ $count = 0 ] ; then
 cd /home/ec2-user/git/develop/trade/tradeApp/service/
 python controller.py
-fi
-
-if [ $count_xrp = 0 ] ; then
-cd /home/ec2-user/git/develop/trade/tradeApp/service/
-python controller_xrp.py
 fi
 
