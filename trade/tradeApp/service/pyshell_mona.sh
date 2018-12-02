67c58a5dd9ee05ec310de1b82c9f39131cbc8af2
shuhei <toto.at.home5@gmail.com>
2018-12-02 10:55:41 +0900

    delete mona

--- trade/tradeApp/service/pyshell_mona.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-count_xrp=`ps aux | grep -v grep | grep controller_mona.py | wc -l`
-
-if [ $count_xrp = 0 ] ; then
-cd /home/ec2-user/git/develop/trade/tradeApp/service/
-python controller_mona.py
-fi
-
